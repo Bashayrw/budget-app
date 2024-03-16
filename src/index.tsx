f35e0a893111d1bfef534246f61101565688ce3d
Bashayr <bashayerali.al-wahbi@integrify.io>
2024-03-16 13:11:18 +0300

    React_Router_added

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/budget-app",
+    element: <App />,
+  },
+]);
 
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <RouterProvider router={router} />
+);
